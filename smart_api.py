73437409febd7d084144e1a94126b4ea84fbe79e
Ashar-khursheed <asharkhursheed26@gmail.com>
2026-05-07 01:17:47 +0500

    Allow all CORS origins for Vercel support

--- smart_api.py
+++ smart_api.py
@@ -17,14 +17,7 @@ app = FastAPI(title="Smart Halal Trading API")
 # Enable CORS for frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173", 
-        "http://127.0.0.1:5173", 
-        "http://localhost:3000", 
-        "http://127.0.0.1:3000",
-        "http://localhost:8000", 
-        "http://127.0.0.1:8000"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
